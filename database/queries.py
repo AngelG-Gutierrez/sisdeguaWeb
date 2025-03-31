4066ec7e20fa14ed30a3244b45b980127bd3982c
Angel Gutierrez <gadea2005@hotmail.com>
2025-03-31 12:42:59 -0600

    ignoreDatabase

--- database/queries.py
+++ database/queries.py
@@ -7,7 +7,7 @@ SUPABASE_URL = 'https://pvbxycfrdbsxhkvdjzyl.supabase.co'
 SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB2Ynh5Y2ZyZGJzeGhrdmRqenlsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk4MjI3NjksImV4cCI6MjA1NTM5ODc2OX0.L-o_wReQ-BWjH17xwiM__18dq1Un13Wf3QJPOToMQh4'
 
 # Crear el cliente de Supabase
-supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY) 
 
 # Funciones para autenticación
 def login_user(email, password):
